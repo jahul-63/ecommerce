7c9e9646995ad4ba91cd9e86a0f6776e8f73c19f
Kuldeep Singh <kuldeep.img123@gmail.com>
2023-02-03 14:51:04 +0530

    work on grid and list view and add routing in footer section

--- src/app/shop-grid/shop-grid.component.ts
+++ src/app/shop-grid/shop-grid.component.ts
@@ -7,9 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShopGridComponent implements OnInit {
 
+  isActive:boolean=true;
+  isActive_1:boolean=false;
+  layout:boolean = true;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  grid_list(event:any){
+    if(event == 'grid'){
+      this.isActive = true;
+      this.isActive_1 = false;
+      this.layout = true;
+    }
+    else if(event == 'list'){
+      this.isActive_1 = true;
+      this.isActive = false;
+      this.layout = false;;
+    }
+    
   }
 
 }
